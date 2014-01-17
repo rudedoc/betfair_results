e052a8edf5d6be848a7b023cb991a8c8f8ffb47b
rudedoc <finlay.mark@gmail.com>
2014-01-17 21:23:55 +0000

    refactored

--- lib/betfair_results.rb
+++ lib/betfair_results.rb
@@ -3,39 +3,39 @@ require 'feedzirra'
 
 module BetfairResults
   class RSS
-
     attr_reader :sport_id, :country_id
-    REGEX_LIST = [/TO BE PLACED/,
-                  /Match Bets/,
-                  /Reverse Forecast /,
-                  /Forecast Betting /,
-                  /Will Racing Go Ahead?/,
-                  /(W\/O)/,
-                  /Daily Win Dist Odds/]
+    REGEX_LIST = Regexp.union([/TO BE PLACED/,
+                               /Match Bets/,
+                               /Reverse Forecast /,
+                               /Forecast Betting /,
+                               /Will Racing Go Ahead?/,
+                               /(W\/O)/,
+                               /Daily Win Dist Odds/])
+
+    def self.find_results_for(codes_hash)
+      new(codes_hash)
+    end
+
+    def outright_winners
+      feed.entries.collect do |entry|
+        clean_summary_string(entry.summary) if !entry.title.match(REGEX_LIST)
+      end.compact
+    end
+
+    private
 
     def initialize(codes_hash)
       @sport_id = codes_hash[:sport_id]
       @country_id = codes_hash[:country_id]
     end
 
-    def self.find_results_for(codes_hash)
-      new(codes_hash)
-    end
-
     def feed
       Feedzirra::Feed.fetch_and_parse("http://rss.betfair.com/RSS.aspx?format=rss&sportID=#{sport_id}&countryID=#{country_id}")
     end
 
-    def outright_winners
-      #TODO: Nasty
-      names = []
-      feed.entries.each do |entry|
-        title = entry.title
-        winner_name = entry.summary if title !~ /TO BE PLACED/ && title !~ /Match Bets/ && title !~ /Reverse Forecast / && title !~ /Forecast Betting / && title !~ /(W\/O)/ && title !~ /Daily Win Dist Odds/ && title !~ /Will Racing Go Ahead?/
-        winner_name.gsub(/\s+/, "") if winner_name != nil
-        names << winner_name.split(': ')[1].downcase if winner_name != nil
-      end
-      names
+    def clean_summary_string(string)
+      string.gsub(/\s+/, "")
+      string.split(': ')[1].downcase.lstrip
     end
   end
 end
--- spec/rss_spec.rb
+++ spec/rss_spec.rb
@@ -5,12 +5,7 @@ require 'spec_helper'
 module BetfairResults
   describe RSS do
     before(:each) do
-      @betfair_rss = RSS.find_results_for({sport_id: 7, country_id: 1})
-    end
-
-
-    it "should get data from BetFair" do
-      expect(@betfair_rss.feed).to be_a_kind_of(Feedzirra::Parser::RSS)
+      @betfair_rss = RSS.find_results_for({sport_id: 7, country_id: 9999})
     end
 
     it "shows me" do
